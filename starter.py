33b2323290fb79e72d6c8b50281c916f13eb7b8a
Abylay <aabylay@gmail.com>
2025-04-15 15:50:18 -0700

    Starting commit for filtered search and changeable config for Milvus

--- starter.py
+++ starter.py
@@ -1,8 +1,13 @@
 import os
+from make_yaml import make_yaml
 
 k_vals = [1, 2, 5, 10, 20, 50, 100]
 
-for k in k_vals:
-    print(f"Running for k: {k}")
-    os.system(f"python run.py --algorithm milvus-hnsw --dataset glove-100-angular --count {k}")
-    print(f"Finished for k: {k}")
+for m in [4, 16, 64]:
+    for k in k_vals:
+        ef_c = m*4
+        ef_s = max(40, k*2)
+        make_yaml(m, ef_c, ef_s)
+        print(f"Running for k: {k}")
+        os.system(f"python run.py --algorithm milvus-hnsw --dataset glove-100-angular --count {k} --filter {filter}")
+        print(f"Finished for k: {k}")
